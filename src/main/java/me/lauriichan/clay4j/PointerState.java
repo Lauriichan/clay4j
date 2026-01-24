ba31f63150b86358d85de49dff7c1d5ec30ac4e1
Lauriichan <lauriichan@playuniverse.org>
2026-01-24 20:59:07 +0100

    Add useful state methods to PointerState

--- src/main/java/me/lauriichan/clay4j/PointerState.java
+++ src/main/java/me/lauriichan/clay4j/PointerState.java
@@ -2,9 +2,36 @@ package me.lauriichan.clay4j;
 
 public enum PointerState {
 
-    PRESSED_THIS_FRAME,
-    PRESSED,
-    RELEASED_THIS_FRAME,
-    RELEASED;
+    PRESSED_THIS_FRAME(true, true),
+    PRESSED(true, false),
+    RELEASED_THIS_FRAME(false, true),
+    RELEASED(false, false);
+
+    private final boolean pressed, thisFrame;
+
+    private PointerState(boolean pressed, boolean thisFrame) {
+        this.pressed = pressed;
+        this.thisFrame = thisFrame;
+    }
+    
+    public boolean hasJustHappened() {
+        return thisFrame;
+    }
+
+    public boolean hasPressed() {
+        return pressed;
+    }
+
+    public boolean hasJustPressed() {
+        return pressed && thisFrame;
+    }
+
+    public boolean hasReleased() {
+        return !pressed;
+    }
+
+    public boolean hasJustReleased() {
+        return !pressed && thisFrame;
+    }
 
 }
